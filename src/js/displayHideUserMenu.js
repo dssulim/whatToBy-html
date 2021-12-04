66bd7ce52713c3a1f1fce16665720d8506b5d0d5
Darya Sulimova <u3tepuka@rambler.ru>
2021-12-05 00:16:12 +0300

    На странице списка магазинов добавила универсальный обработчик открытия меню по нажатию на соответствующие кнопки, и для закрытия по нажатию в любом месте или на пункт меню

--- src/js/displayHideUserMenu.js
+++ src/js/displayHideUserMenu.js
@@ -1,12 +1,51 @@
-window.addEventListener('DOMContentLoaded', function() {
-    let headerRight = document.querySelector(".header-right");
-    let shimUserMenu = document.querySelector(".shim-user-menu");
-    headerRight.addEventListener("click", function(event){
+function displayHideUserMenu(myParentElement, shim, userMenu, userMenuItem){
+    //console.log(myParentElement, shim, userMenu, userMenuItem);
+    const shimUserMenu = document.querySelector("." + shim);
+    const targetClosestUserMenu = "." + userMenu;
+    const targetClosestUserMenuItem = "." + userMenuItem;
+    const shimMenu = "." + shim;
+    const parentElement = document.querySelector("." + myParentElement);
+    shimUserMenu.classList.add("shim-modal-show");
+
+    parentElement.addEventListener("click", function(event){
         let target = event.target;
-        if(target.closest(".user-menu") && !target.closest(".user-menu-item"))
+        if(target.closest(targetClosestUserMenu) && !target.closest(targetClosestUserMenuItem)) {
             event.stopPropagation();
-        else if( target.closest(".shim-user-menu") )
+        }
+        else if( target.closest(shimMenu) ) {
             shimUserMenu.classList.remove("shim-modal-show");
-        else shimUserMenu.classList.add("shim-modal-show");
+        }
+        else {
+            shimUserMenu.classList.add("shim-modal-show");
+        }
     });
-});
+}
+
+// window.addEventListener('DOMContentLoaded', function() {
+//     let headerRight = document.querySelector(".header-right");
+//     let shimUserMenu = document.querySelector(".shim-user-menu");
+//
+//     headerRight.addEventListener("click", function(event){
+//         // console.log(event.target);
+//         let target = event.target;
+//         if(target.closest(".user-menu") && !target.closest(".user-menu-item"))
+//             event.stopPropagation();
+//         else if( target.closest(".shim-user-menu") )
+//             shimUserMenu.classList.remove("shim-modal-show");
+//         else shimUserMenu.classList.add("shim-modal-show");
+//     });
+//
+//     let settingsButton = document.querySelector(".settings-button");
+//     let shimUserMenuSettings = document.querySelector(".shim-user-menu-settings");
+//     //console.log(shimUserMenuSettings); // null
+//
+//     settingsButton.addEventListener("click", function(event){
+//         //console.log(event.target);
+//         let target = event.target;
+//         if(target.closest(".user-menu-settings") && !target.closest(".user-menu-item-s"))
+//             event.stopPropagation();
+//         else if( target.closest(".shim-user-menu-settings") )
+//             shimUserMenuSettings.classList.remove("shim-modal-show-s");
+//         else shimUserMenuSettings.classList.add("shim-modal-show-s");
+//     });
+// });
